9a61aa11ededa95d9872cdd92664288012cd96a9
Marc Perrien <marc@chaosiq.io>
2021-01-22 10:17:32 +0100

    Markdown gen (#22)
    * added link handler & file prepender
    
    Signed-off-by: gtfisher <grant@chaosiq.io>
    
    * Update front matter
    
    Signed-off-by: Marc Perrien <marc@chaosiq.io>
    
    * Fix front matter excerpt.
    
    Signed-off-by: Marc Perrien <marc@chaosiq.io>
    
    * Fix typo.
    Remove commented code.
    
    Signed-off-by: Marc Perrien <marc@chaosiq.io>
    
    Co-authored-by: gtfisher <grant@chaosiq.io>

--- cmd/doc/markdown.go
+++ cmd/doc/markdown.go
@@ -1,6 +1,11 @@
 package doc
 
 import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
@@ -8,13 +13,35 @@ import (
 func NewMarkdownCommand(rootCmd *cobra.Command) *cobra.Command {
 	var outputDirectory string = "."
 
+	const baseURLPath = "/docs/reference/cli/"
+
+	linkHandler := func(name string) string {
+		base := strings.TrimSuffix(name, path.Ext(name))
+		return baseURLPath + strings.ToLower(strings.Replace(base, "_", "-", -1)) + "/"
+	}
+
+	const fmTemplate = `---
+title: %s
+excerpt: Documentation for the %s command in the Reliably CLI
+categories: ["reference", "cli"]
+status: published
+type: doc
+---
+`
+
+	filePrepender := func(filename string) string {
+		name := filepath.Base(filename)
+		base := strings.TrimSuffix(name, path.Ext(name))
+		return fmt.Sprintf(fmTemplate, strings.Replace(base, "_", " ", -1), filename)
+	}
+
 	cmd := &cobra.Command{
 		Use:   "markdown",
 		Args:  cobra.ExactArgs(0),
 		Short: "Generates Markdown pages for the reliably CLI.",
 		Long:  "Generate one Markdown document per command.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return doc.GenMarkdownTree(rootCmd, outputDirectory)
+			return doc.GenMarkdownTreeCustom(rootCmd, outputDirectory, filePrepender, linkHandler)
 		},
 	}
 
