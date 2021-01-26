4369e55bc0ef07aa5da623e870d9d638259f10c2
David Martin <david@chaosiq.io>
2021-01-26 19:33:25 +0100

    Ensure the default version is DEV when no version is given (when building docker image whith empty args)
    Signed-off-by: David Martin <david@chaosiq.io>

--- version/version.go
+++ version/version.go
@@ -4,16 +4,23 @@ import (
 	"runtime/debug"
 )
 
+const devVersion = "DEV"
+
 // Version of the CLI with semver format
 // Version is dynamically set by the toolchain or overridden by the Makefile.
-var Version = "DEV"
+var Version = devVersion
 
 // Date is dynamically set at build time in the Makefile.
 var Date = "" // YYYY-MM-DD
 
 func init() {
-	if Version == "DEV" {
-		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" {
+
+	if Version == "" {
+		Version = devVersion
+	}
+
+	if Version == devVersion {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" && info.Main.Version != "" {
 			Version = info.Main.Version
 		}
 	}
