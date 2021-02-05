53db66b908d1fb92119cdf9cfa82a5a80bc8a347
David Martin <david@chaosiq.io>
2021-02-05 16:16:00 +0100

    Add contructor for version command
    Signed-off-by: David Martin <david@chaosiq.io>

--- cmd/reliably/version.go
+++ cmd/reliably/version.go
@@ -8,17 +8,21 @@ import (
 )
 
 func init() {
-	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(NewCmdVersion())
 }
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of the Reliably CLI",
-	Long:  `Print the version number of the Reliably CLI`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print(FormatVersion(version, buildDate))
-	},
-	Hidden: true, // version will not be indicated as command but only flag
+func NewCmdVersion() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of the Reliably CLI",
+		Long:  `Print the version number of the Reliably CLI`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Print(FormatVersion(version, buildDate))
+		},
+		Hidden: true, // version will not be indicated as command but only flag
+	}
+
+	return cmd
 }
 
 // FormatVersion returns a formatted string with Reliably CLI's version
