b524b84f2ebc6ca0d1006f92ce92293b0baf59ae
David Martin <david@chaosiq.io>
2021-02-10 10:59:36 +0100

    Fix assertion equal variables order (expected before actual)
    Signed-off-by: David Martin <david@chaosiq.io>

--- utils/git_test.go
+++ utils/git_test.go
@@ -8,17 +8,17 @@ import (
 
 func TestTopLevelDir(t *testing.T) {
 	d, err := ToplevelDir()
-	assert.Equal(t, err, nil, "Unexpected error")
-	assert.NotEqual(t, d, "", "Top level dir should not be empty")
+	assert.Equal(t, nil, err, "Unexpected error")
+	assert.NotEqual(t, "", d, "Top level dir should not be empty")
 }
 
 func TestGitDir(t *testing.T) {
 	d, err := GitDir()
-	assert.Equal(t, err, nil, "Unexpected error")
-	assert.NotEqual(t, d, "", "Git dir should not be empty")
+	assert.Equal(t, nil, err, "Unexpected error")
+	assert.NotEqual(t, "", d, "Git dir should not be empty")
 }
 
 func TestIsGitRepo(t *testing.T) {
 	b := IsGitRepo()
-	assert.Equal(t, b, true, "The CLI is a git repo !")
+	assert.Equal(t, true, b, "The CLI is a git repo !")
 }
