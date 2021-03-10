99037bee39a1f3757096386e8585eb3757aadc59
David Martin <david@chaosiq.io>
2021-03-10 16:49:26 +0100

    Make example field not exportable to json or yaml
    Signed-off-by: David Martin <david@chaosiq.io>

--- core/suggestion.go
+++ core/suggestion.go
@@ -16,9 +16,9 @@ type Suggestion struct {
 	Level   Level  `json:"level"`           // level
 	File    string `json:"file"`            // File name we found it in
 	//Code       string `json:"code"`       // Impacted code line
-	Line    int    `json:"line"`    // Line number in file
-	Col     int    `json:"column"`  // Column number in line
-	Example string `json:"example"` // Example of valid rule usage
+	Line    int    `json:"line"`       // Line number in file
+	Col     int    `json:"column"`     // Column number in line
+	Example string `json:"-" yaml:"-"` // Example of valid rule usage
 
 	Platform string `json:"platform"` // Platform handling the resource
 	Kind     string `json:"type"`     // Type of resource
