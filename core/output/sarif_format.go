ea862846fd3005ca863c2dbeb5cbe5a63bf9714a
David Martin <david@chaosiq.io>
2021-03-11 18:27:15 +0100

    restore markdown for rule help - change format a bit
    Signed-off-by: David Martin <david@chaosiq.io>

--- core/output/sarif_format.go
+++ core/output/sarif_format.go
@@ -54,7 +54,7 @@ type sarifMessage struct {
 
 type sarifMultiformatMessage struct {
 	Text     string `json:"text"`
-	Markdown string `json:"-"`
+	Markdown string `json:"markdown"`
 }
 
 type sarifResult struct {
@@ -113,7 +113,7 @@ func buildSarifRule(suggestion *core.Suggestion) *sarifRule {
 	if suggestion.Example != "" {
 		help = &sarifMultiformatMessage{
 			Text:     fmt.Sprintf("Example:\n%s", suggestion.Example),
-			Markdown: fmt.Sprintf("# Example:\n```%s```", suggestion.Example),
+			Markdown: fmt.Sprintf("Example:\n```%s```", suggestion.Example),
 		}
 	}
 
