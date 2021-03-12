974864c6ecaf7e5f88965a5e0d3513e499338b23
daidokoro <shaun@daidokoro.io>
2021-03-12 13:53:45 +0000

    added subgroup property to Target type
    added comments
    
    implemented String method for Rule type

--- scan/types.go
+++ scan/types.go
@@ -1,23 +1,34 @@
 package scan
 
+import "encoding/json"
+
+// Target - struct represents the entity/element to be
+// scanned/evaluated
 type Target struct {
 	ResourceType string
 	Platform     string
 	Item         interface{}
 	Metadata     map[string]string
-	Result       Result
+	subgroups    []string
 }
 
+// Result - stores result of scan evaluation
 type Result struct {
 	Suggestions []string
 	Violations  []Rule
 }
 
+// Rule - type used to unmarshal Rego Policy violations[rule]
 type Rule struct {
 	Level          uint32 `json:"level"`
 	Message        string `json:"message"`
 	RuleID         string `json:"ruleID"`
 	RuleDefinition string `json:"ruleDef"`
+}
+
+func (r Rule) String() string {
+	b, _ := json.MarshalIndent(&r, "", "  ")
+	return string(b)
 }
 
 type policy struct {
