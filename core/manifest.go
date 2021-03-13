d5cb57e8a34831cac07137e9f6d9af7c2d6d2360
mick-roper <mick@theropers.co.uk>
2021-03-13 10:28:05 +0000

    added a simple advice system

---
+++ core/manifest.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+const defaultManifestPath = "relably.yaml"
+
+// Manifest that describes a Reliably applciation
+type Manifest struct {
+	Type     string
+	Platform string
+}
+
+func LoadManifest() (*Manifest, error) {
+	path := getManifestPath()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var m Manifest
+
+	if err := yaml.NewDecoder(file).Decode(&m); err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+func getManifestPath() string {
+	return defaultManifestPath
+}
