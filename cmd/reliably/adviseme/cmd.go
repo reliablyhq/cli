8588e47aedc961596b8330e6f2ea658d64f2c34f
mick-roper <mick@theropers.co.uk>
2021-03-13 10:28:05 +0000

    added a simple advice system

--- cmd/reliably/adviseme/cmd.go
+++ cmd/reliably/adviseme/cmd.go
@@ -1,7 +1,11 @@
 package adviseme
 
 import (
-	"github.com/sirupsen/logrus"
+	"os"
+
+	"github.com/reliablyhq/cli/core"
+	"github.com/reliablyhq/cli/core/advice"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -14,5 +18,36 @@ func NewCommand() *cobra.Command {
 }
 
 func run(_ *cobra.Command, _ []string) {
-	logrus.Fatal("not implemented")
+	m, err := core.LoadManifest()
+	if err != nil {
+		log.Debug(err)
+		log.Fatal("An error occured while attempting to load the manifest")
+	}
+
+	allAdvice := make([]*advice.Advice, 0)
+
+	if a, err := advice.GetAdviceFor(m.Type); err != nil {
+		log.Debug(err)
+		log.Fatalf("an error occured while trying to get advice for '%s'", m.Type)
+	} else {
+		allAdvice = append(allAdvice, a)
+	}
+
+	if a, err := advice.GetAdviceFor(m.Platform); err != nil {
+		log.Debug(err)
+		log.Fatalf("an error occured while trying to get advice for '%s'", m.Type)
+	} else {
+		allAdvice = append(allAdvice, a)
+	}
+
+	if len(allAdvice) == 0 {
+		log.Warn("no advice was found - how odd....")
+		os.Exit(0)
+	}
+
+	for _, a := range allAdvice {
+		for _, s := a.Suggestions {
+			log.Infof("%s -> %s", a.Type, s)
+		}
+	}
 }
---
+++ core/advice/types.go
@@ -0,0 +1,10 @@
+package advice
+
+type Advice struct {
+	Type        string
+	Suggestions []string
+}
+
+func GetAdviceFor(topic string) (*Advice, error) {
+
+}
