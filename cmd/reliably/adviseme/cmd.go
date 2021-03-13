0895a8205e252a5cdc899fc631af24bdf5724ad5
mick-roper <mick@theropers.co.uk>
2021-03-13 15:54:51 +0000

    added a mechanism to gett advice for a certain platform

--- cmd/reliably/adviseme/cmd.go
+++ cmd/reliably/adviseme/cmd.go
@@ -1,8 +1,6 @@
 package adviseme
 
 import (
-	"os"
-
 	"github.com/reliablyhq/cli/core"
 	"github.com/reliablyhq/cli/core/advice"
 	log "github.com/sirupsen/logrus"
@@ -24,29 +22,14 @@ func run(_ *cobra.Command, _ []string) {
 		log.Fatal("An error occured while attempting to load the manifest")
 	}
 
-	allAdvice := make([]*advice.Advice, 0)
-
-	if a, err := advice.GetAdviceFor(m.Type); err != nil {
+	allAdvice, err := advice.GetAdviceFor(m)
+	if err != nil {
 		log.Debug(err)
-		log.Fatalf("an error occured while trying to get advice for '%s'", m.Type)
-	} else {
-		allAdvice = append(allAdvice, a)
-	}
-
-	if a, err := advice.GetAdviceFor(m.Platform); err != nil {
-		log.Debug(err)
-		log.Fatalf("an error occured while trying to get advice for '%s'", m.Type)
-	} else {
-		allAdvice = append(allAdvice, a)
-	}
-
-	if len(allAdvice) == 0 {
-		log.Warn("no advice was found - how odd....")
-		os.Exit(0)
+		log.Fatal("an error occured while geting advice")
 	}
 
 	for _, a := range allAdvice {
-		for _, s := a.Suggestions {
+		for _, s := range a.Suggestions {
 			log.Infof("%s -> %s", a.Type, s)
 		}
 	}
