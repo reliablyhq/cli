4b336d1e033ed91824ce671c0f36a7ed38ec1cda
mick-roper <mick@theropers.co.uk>
2021-03-16 13:58:23 +0000

    dependencies have been added to the report

--- core/report/report.go
+++ core/report/report.go
@@ -1,15 +1,22 @@
 package report
 
-import "time"
+import (
+	"time"
+
+	"github.com/reliablyhq/cli/core/manifest"
+)
 
 type Report struct {
-	ApplicationName string    `json:"application_name"`
-	Timestamp       time.Time `json:"timestamp"`
-	Targets         struct {
+	ApplicationName string                 `json:"application_name"`
+	Timestamp       time.Time              `json:"timestamp"`
+	Dependencies    []*manifest.Dependency `json:"dependencies"`
+
+	Targets struct {
 		ServiceLevel       float32       `json:"service_level"`
 		ErrorBudgetPercent float32       `json:"error_budget"`
 		Latency            time.Duration `json:"latency"`
 	} `json:"target"`
+
 	Delta struct {
 		ServiceLevel       float32       `json:"service_level"`
 		ErrorBudgetPercent float32       `json:"error_budget_pc"`
