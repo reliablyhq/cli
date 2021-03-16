243e7689563d2446be74a6a9c65879df4a8ee547
mick-roper <mick@theropers.co.uk>
2021-03-16 09:45:49 +0000

    added some percentage formatting

--- core/report/service_level.go
+++ core/report/service_level.go
@@ -12,8 +12,8 @@ const (
 	overAvailbilityThresholdPercent   = 2
 	lessThan95pcAvailabilityMessage   = "An availability of less than 95% allows more than 36.5 hours of downtime per month, which should be possible for any well built app deployed as a single instance. This availability target is probably not high enough for a production-ready system."
 	moreThan9999pcAvailabilityMessage = "An availabiliy of more than 99.99% is possible, but is quite expensive and may be unnecessary. 99.99% availability allows 4.38 minutes of downtime per month. You should make sure that less downtime is a requirement of your service."
-	actualAvailabilityTooLowf         = "Current availability is lower than target availability by %v percent. Think about trying to increase the resources allocated of your application"
-	actualAvailabilityTooHighf        = "Current availability is higher than target availability by %v percent. Think about reducing the resources allocated to your application - this could save you some money."
+	actualAvailabilityTooLowf         = "Current availability is lower than target availability by %.2f percent. Think about trying to increase the resources allocated of your application"
+	actualAvailabilityTooHighf        = "Current availability is higher than target availability by %.2f percent. Think about reducing the resources allocated to your application - this could save you some money."
 )
 
 func getSuggestionsForServiceLevel(m *manifest.Manifest) ([]Suggestion, error) {
