62c6f21ab7bdee436be4b4ac2c91f46aadaa03f7
mick-roper <mick@theropers.co.uk>
2021-03-16 09:06:55 +0000

    updated the availability message

--- core/report/service_level.go
+++ core/report/service_level.go
@@ -5,11 +5,11 @@ import (
 )
 
 const (
-	lessThan95pcAvailabilityMessage   = "An availability of less than 95% should be possible for any well built app, deployed  as a single instance"
+	lessThan95pcAvailabilityMessage   = "An availability of less than 95% allows more than 36.5 hours of downtime per month, which should be possible for any well built app deployed as a single instance"
 	moreThan9999pcAvailabilityMessage = "An availabiliy of more than 99.99% is possible, but is quite expensive and may be unnecessary. 99.99% availability allows 4.38 minutes of downtime per month. You should make sure that less downtime is a requirement of your service."
 )
 
-func getSuggestionsForService(m *manifest.Manifest) ([]Suggestion, error) {
+func getSuggestionsForServiceLevel(m *manifest.Manifest) ([]Suggestion, error) {
 	suggestions := make([]Suggestion, 0)
 
 	if m.ServiceLevel.Availability < 95 {
