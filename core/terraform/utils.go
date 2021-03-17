aeb64729b4974b9839998d9588789f30a781c7a1
daidokoro <shaun@daidokoro.io>
2021-03-17 11:50:59 +0000

    moved TF file unmarshaller to core utils

--- core/terraform/utils.go
+++ core/terraform/utils.go
@@ -1,8 +1,14 @@
 package terraform
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
+	"github.com/hashicorp/hcl"
 	"github.com/reliablyhq/cli/core"
 )
 
@@ -28,4 +34,34 @@ func ExtractResourcesFromPlan(plan *PlanRepresentation) ([]*core.Resource, error
 	}
 
 	return resources, nil
+}
+
+// UnmarshalTF - unmarshals .tf files by recursively removing
+// unsupported HCL lines, for eg. terraform functions. The theory being that
+// we'll only be interested in policing values we can directly support
+func UnmarshalTF(tf []byte, v interface{}) error {
+	var re = regexp.MustCompile(`Unknown token\:.(\d{1,5}):`)
+
+	if err := hcl.Unmarshal(tf, v); err != nil {
+		if !re.MatchString(err.Error()) {
+			return err
+		}
+
+		ln := re.FindAllStringSubmatch(err.Error(), -1)[0][1]
+		badlineNo, _ := strconv.Atoi(ln)
+
+		var updated strings.Builder
+		var lineNo = 1
+		scanner := bufio.NewScanner(strings.NewReader(string(tf)))
+
+		// remove lineNo
+		for scanner.Scan() {
+			if lineNo != badlineNo {
+				fmt.Fprintln(&updated, scanner.Text())
+			}
+			lineNo++
+		}
+		return UnmarshalTF([]byte(updated.String()), v)
+	}
+	return nil
 }
