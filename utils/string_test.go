a87ecbc3250474001e6ff6a673cbf4ae0d2568a4
gtfisher <grant@chaosiq.io>
2021-03-17 16:46:15 +0000

    adjusted truncate to account for special characters and tidied truncate function
    Signed-off-by: gtfisher <grant@chaosiq.io>

--- utils/string_test.go
+++ utils/string_test.go
@@ -22,11 +22,81 @@ func TestRandomString(t *testing.T) {
 func TestTruncateStringTo20(t *testing.T) {
 	const s = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 	Pellentesque faucibus a ante at viverra.`
+	expectedStr := "Lorem ipsum dolor..."
 	truncStr := TruncateString(s, 20)
-	if len(truncStr) != 20 {
+	if len(truncStr) != len(expectedStr) {
 		t.Error("Truncated string does not have expected length")
 	}
-	if truncStr != "Lorem ipsum dolor..." {
+	if truncStr != expectedStr {
+		t.Error("Truncated string match the epxected string")
+	}
+	t.Logf("Truncated string:\n%s", truncStr)
+
+}
+
+func TestTruncateShortStringDoesNotAddEllipse(t *testing.T) {
+	const s = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	Pellentesque faucibus a ante at viverra.`
+	expectedStr := "Lor"
+	truncStr := TruncateString(s, 3)
+	if len(truncStr) != len(expectedStr) {
+		t.Error("Truncated string does not have expected length")
+	}
+	if truncStr != expectedStr {
+		t.Error("Truncated string match the epxected string")
+	}
+	t.Logf("Truncated string:\n%s", truncStr)
+
+}
+
+func TestDoesNotTruncateLessThan20(t *testing.T) {
+	const s = `Lorem ipsum dolor si`
+	expectedStr := "Lorem ipsum dolor si"
+	truncStr := TruncateString(s, 20)
+	if len(truncStr) != len(expectedStr) {
+		t.Error("Truncated string does not have expected length")
+	}
+	if truncStr != expectedStr {
+		t.Error("Truncated string match the epxected string")
+	}
+	t.Logf("Truncated string:\n%s", truncStr)
+
+}
+
+func TestTruncatesColoredStringWithSymbol(t *testing.T) {
+
+	// define string in bytes as it has control codes and special characters ("■")
+	ba := []byte{27, 91, 51, 51, 109, 226, 150, 160, 27, 91, 48, 109, 32, 32, 101, 120, 97, 109, 112, 108, 101, 58, 48, 58, 48, 101, 120, 97, 109, 112, 108, 101, 58, 48, 58, 48}
+
+	sourceStr := string(ba)
+
+	expectedString := string(ba[0:25]) + "..."
+
+	truncStr := TruncateString(sourceStr, 17)
+	if len(truncStr) != len(expectedString) {
+		t.Error("Truncated string does not have expected length")
+	}
+	if truncStr != expectedString {
+		t.Error("Truncated string match the epxected string")
+	}
+	t.Logf("Truncated string:\n%s", truncStr)
+
+}
+
+func TestTruncatesShortColoredStringWithSymbolDoesNotAddEllipse(t *testing.T) {
+
+	// define string in bytes as it has control codes and special characters ("■")
+	ba := []byte{27, 91, 51, 51, 109, 226, 150, 160, 27, 91, 48, 109, 32, 32, 101, 120, 97, 109, 112, 108, 101, 58, 48, 58, 48, 101, 120, 97, 109, 112, 108, 101, 58, 48, 58, 48}
+
+	sourceStr := string(ba)
+
+	expectedString := string(ba[0 : 11+3])
+
+	truncStr := TruncateString(sourceStr, 3)
+	if len(truncStr) != len(expectedString) {
+		t.Error("Truncated string does not have expected length")
+	}
+	if truncStr != expectedString {
 		t.Error("Truncated string match the epxected string")
 	}
 	t.Logf("Truncated string:\n%s", truncStr)
