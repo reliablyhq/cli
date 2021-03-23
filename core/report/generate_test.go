b60f1dca3e9dfef6d1d269cd91c2831c2f955f38
mick-roper <mick@theropers.co.uk>
2021-03-23 15:55:40 +0000

    refactoring the test

--- core/report/generate_test.go
+++ core/report/generate_test.go
@@ -5,22 +5,28 @@ import (
 	"testing"
 	"time"
 
+	"github.com/reliablyhq/cli/core/manifest"
 	"github.com/reliablyhq/cli/core/metrics"
 )
 
-type dummyProvider struct{}
+type dummyProvider struct {
+	latencyMetricValue float64
+	latencyError       error
+	errorPercentValue  float64
+	errorPercentError  error
+}
 
 func (p *dummyProvider) Get99PercentLatencyMetricForResource(resourceID string, from, to time.Time) (float64, error) {
-	return -1, nil
+	return p.latencyMetricValue, p.latencyError
 }
 
 func (p *dummyProvider) GetErrorPercentageMetricForResource(resourceID string, from, to time.Time) (float64, error) {
-	return -1, nil
+	return p.errorPercentValue, p.errorPercentError
 }
 
 func Test_getProviderForResource(t *testing.T) {
 	p := &dummyProvider{}
-	metrics.ProviderFactories["dummy"] = func() (metrics.Provider, error) { return p, nil }
+	metrics.ProviderFactories["test_get_provider_for_resource"] = func() (metrics.Provider, error) { return p, nil }
 
 	type args struct {
 		ID string
@@ -33,7 +39,7 @@ func Test_getProviderForResource(t *testing.T) {
 	}{
 		{
 			name:    "returns the correct provider",
-			args:    args{ID: "dummy/123"},
+			args:    args{ID: "test_get_provider_for_resource/123"},
 			want:    p,
 			wantErr: false,
 		},
@@ -59,6 +65,48 @@ func Test_getProviderForResource(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getProviderForResource() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFromManifest(t *testing.T) {
+	p := &dummyProvider{}
+	metrics.ProviderFactories["test_from_manifest"] = func() (metrics.Provider, error) { return p, nil }
+
+	type args struct {
+		m *manifest.Manifest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Report
+		wantErr bool
+	}{
+		{
+			name:    "happy path",
+			args:    args{},
+			want:    &Report{},
+			wantErr: false,
+		},
+		{
+			name: "returns error if manifest is nil",
+			args: args{
+				m: nil,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FromManifest(tt.args.m)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromManifest() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FromManifest() = %v, want %v", got, tt.want)
 			}
 		})
 	}
