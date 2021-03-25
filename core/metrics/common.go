bfd6acfd60a1f082829e374a808296671b902ad9
mick-roper <mick@theropers.co.uk>
2021-03-25 10:26:07 +0000

    Added a dummy ptovider

--- core/metrics/common.go
+++ core/metrics/common.go
@@ -1,10 +1,15 @@
 package metrics
 
 import (
+	"math/rand"
 	"time"
 )
 
-var ProviderFactories = map[string]ProviderFactory{}
+var r = rand.New(rand.NewSource(time.Now().Unix()))
+
+var ProviderFactories = map[string]ProviderFactory{
+	"dummy": func() (Provider, error) { return &DummyProvider{}, nil },
+}
 
 type (
 	ProviderFactory func() (Provider, error)
@@ -14,3 +19,13 @@ type (
 		GetErrorPercentageMetricForResource(resourceID string, from, to time.Time) (float64, error)
 	}
 )
+
+type DummyProvider struct{}
+
+func (d *DummyProvider) Get99PercentLatencyMetricForResource(resourceID string, from, to time.Time) (float64, error) {
+	return 500 * r.Float64(), nil
+}
+
+func (d *DummyProvider) GetErrorPercentageMetricForResource(resourceID string, from, to time.Time) (float64, error) {
+	return 25 * r.Float64(), nil
+}
