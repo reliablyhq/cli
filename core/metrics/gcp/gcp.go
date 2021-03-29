b5849e58459d48f20e03867ba0e17f6e62a10c4e
mick-roper <mick@theropers.co.uk>
2021-03-29 13:41:57 +0100

    added anonymous variables to provide cheap type checking

--- core/metrics/gcp/gcp.go
+++ core/metrics/gcp/gcp.go
@@ -9,6 +9,7 @@ import (
 	monitoring "cloud.google.com/go/monitoring/apiv3"
 	"github.com/golang/protobuf/ptypes/duration"
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/reliablyhq/cli/core/metrics"
 	"google.golang.org/api/iterator"
 	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
 )
@@ -17,6 +18,9 @@ type GCP struct {
 	ctx    context.Context
 	client *monitoring.MetricClient
 }
+
+// type checking
+var _ metrics.Provider = &GCP{}
 
 // Currently needs a service account configured or gcloud auth within same session
 func NewGCP() (*GCP, error) {
