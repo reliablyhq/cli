d39aa0091f33be03d63a8a6e49900aa1dd9bff0b
Xavier Armijos <xaah@users.noreply.github.com>
2021-03-29 13:18:21 +0100

    gcp metrics (#122)
    * initial gcp auth
    
    * 99 percent latency no resource name
    
    * integrated gcp provider
    
    * WIP:added body for GetErrorPercentageMetricForResource
    
    Signed-off-by: gtfisher <grant@chaosiq.io>
    
    * refactor for testing
    
    * refactored for testing
    
    Co-authored-by: gtfisher <grant@chaosiq.io>

---
+++ core/metrics/gcp_test.go
@@ -0,0 +1,15 @@
+package metrics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseLatency(t *testing.T) {
+	assert.Equal(t, nil, nil, "Unexpected error")
+}
+
+func TestParseStatusErrors(t *testing.T) {
+	assert.Equal(t, nil, nil, "Unexpected error")
+}
