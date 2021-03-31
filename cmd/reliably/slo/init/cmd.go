0a2086c4429ed90785a6dd24a6f3b18ca3342d95
David Martin <david@chaosiq.io>
2021-03-31 14:57:05 +0200

    fix panic error, missing default struct for service level in manifest object
    Signed-off-by: David Martin <david@chaosiq.io>

--- cmd/reliably/slo/init/cmd.go
+++ cmd/reliably/slo/init/cmd.go
@@ -77,6 +77,7 @@ func validateFilePath() error {
 }
 
 func populateManifestInteractively(m *manifest.Manifest, scanner *bufio.Scanner) {
+	m.ServiceLevel = &manifest.Service{}
 	if question.WithBoolAnswer(scanner, "Are you building something that will be provided to customers 'as a service'?") {
 		m.ServiceLevel.Objective = manifest.ServiceLevelObjective{
 			ErrorBudgetPercent: question.WithFloat64Answer(scanner, "What percentage of requests to your service is it ok to have fail? This will be your 'error budget'.", 0, 100),
