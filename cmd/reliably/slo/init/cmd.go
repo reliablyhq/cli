b987aa240abc5c0529677eaec3e22181530f37d2
David Martin <david@chaosiq.io>
2021-03-31 15:06:36 +0200

    fix the 'slo init' help usage
    Signed-off-by: David Martin <david@chaosiq.io>

--- cmd/reliably/slo/init/cmd.go
+++ cmd/reliably/slo/init/cmd.go
@@ -102,16 +102,11 @@ func populateManifestInteractively(m *manifest.Manifest, scanner *bufio.Scanner)
 
 func longCommandDescription() string {
 	return heredoc.Doc(`
-		Initialise the reliably manifest.
+Initialise the reliably manifest.
 
-		The manifest describes the operational contraints of the application, as well as some metadata about the app that allows users to reach out and communicate with the maintainer.
-
-		Usage:
-		1. reliably slo init: this method interactively creates a manifest file, asking you questions on the command line and adding your answers to the manifest file.
-		2. realibly slo init -f <path>: this method works the same as reliably init, but allows you to specify the location of the file. This is useful if you use a multi-repo approach to source control.
-		The manifest describes the operational contraints of the application,
-		as well as some metadata about the app that allows users to reach out
-		and communicate with the maintainer.`)
+The manifest describes the operational contraints of the application,
+as well as some metadata about the app that allows users to reach out
+and communicate with the maintainer.`)
 }
 
 func examples() string {
