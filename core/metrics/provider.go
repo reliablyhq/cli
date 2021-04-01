dbd244749a1a72be7f9837827227af2659b726df
Mick Roper <mick@theropers.co.uk>
2021-04-01 09:41:35 +0100

    removed the dummy provider (#138)
    * removed the dummy provider
    
    * removed the random var

--- core/metrics/provider.go
+++ core/metrics/provider.go
@@ -1,15 +1,11 @@
 package metrics
 
 import (
-	"math/rand"
 	"time"
 
 	"github.com/reliablyhq/cli/core/metrics/aws"
 	"github.com/reliablyhq/cli/core/metrics/gcp"
-	"github.com/reliablyhq/cli/version"
 )
-
-var r = rand.New(rand.NewSource(time.Now().Unix()))
 
 var ProviderFactories = map[string]ProviderFactory{
 	"aws": func() (Provider, error) { return aws.NewAwsCloudWatch() },
@@ -24,9 +20,3 @@ type (
 		GetErrorPercentageMetricForResource(resourceID string, from, to time.Time) (float64, error)
 	}
 )
-
-func init() {
-	if version.IsDevVersion() {
-		ProviderFactories["dummy"] = func() (Provider, error) { return &DummyProvider{}, nil }
-	}
-}
