8c2a975f8d6afdb485a9095c5d1a2c6be579b1ba
David Martin <david@chaosiq.io>
2021-04-02 11:34:18 +0200

    - add missing valid latency slo message - fixes #146 - change simple format to provide actual values
    Signed-off-by: David Martin <david@chaosiq.io>

--- core/report/writer.go
+++ core/report/writer.go
@@ -7,15 +7,17 @@ import (
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/reliablyhq/cli/core/color"
+	"github.com/reliablyhq/cli/core/iostreams"
 	"github.com/sirupsen/logrus"
 )
 
 const (
 	threshold                       = 0
 	lessThan95pcAvailabilityMessage = "An availability of less than 95% allows more than 36.5 hours of downtime per month, which should be possible for any well built app deployed as a single instance. This availability target is probably not high enough for a production-ready system."
-	errorBudgetExceededf            = "Your error budget has been exceeded by %.2f percent. This is pretty bad :("
+	errorBudgetExceededf            = "Your error budget has been exceeded by %.2f percent."
 	errorBudgetTooLowf              = "You are under your error budget by %.2f percent. You could tighten your budget, or could decrease the quality of the experience your application provides (e.g by reducing the amount of resources given to your application)."
-	latencyExceeded                 = "The average latency threshold has been exceeeded by %vms"
+	latencyExceeded                 = "The average latency threshold has been exceeeded by %vms."
+	latencyValid                    = "The average latency threshold has not been exceeded."
 )
 
 const (
@@ -54,14 +56,21 @@ func Write(format Format, r *Report, w io.Writer, l *logrus.Logger) {
 		fmt.Fprintln(w, string(b))
 
 	case SimpleText:
-		if r.ServiceLevel.Delta.ErrorPercent < threshold {
-			l.Warnf(errorBudgetTooLowf, -r.ServiceLevel.Delta.ErrorPercent)
-		} else if r.ServiceLevel.Delta.ErrorPercent > threshold {
-			l.Warnf(errorBudgetExceededf, r.ServiceLevel.Delta.ErrorPercent)
+		fmt.Printf("SLO report: (last %s)\n", r.ObservationWindow.To.Sub(r.ObservationWindow.From))
+
+		if r.ServiceLevel.Delta.ErrorPercent > threshold {
+			msg := fmt.Sprintf(errorBudgetExceededf, r.ServiceLevel.Delta.ErrorPercent)
+			fmt.Printf("%s Error Rate: %.2f%%. %s\n", iostreams.FailureIcon(), r.ServiceLevel.Actual.ErrorPercent, msg)
+		} else {
+			msg := fmt.Sprintf(errorBudgetTooLowf, -r.ServiceLevel.Delta.ErrorPercent)
+			fmt.Printf("%s Error Rate: %.2f%%. %s\n", iostreams.SuccessIcon(), r.ServiceLevel.Actual.ErrorPercent, msg)
 		}
 
 		if r.ServiceLevel.Delta.LatencyMs > threshold {
-			l.Warnf(latencyExceeded, r.ServiceLevel.Delta.LatencyMs)
+			msg := fmt.Sprintf(latencyExceeded, r.ServiceLevel.Delta.LatencyMs)
+			fmt.Printf("%s Latency: %vms. %s\n", iostreams.FailureIcon(), r.ServiceLevel.Actual.LatencyMs, msg)
+		} else {
+			fmt.Printf("%s Latency: %vms. %s\n", iostreams.SuccessIcon(), r.ServiceLevel.Actual.LatencyMs, latencyValid)
 		}
 
 	default:
@@ -71,7 +80,7 @@ func Write(format Format, r *Report, w io.Writer, l *logrus.Logger) {
 }
 
 func tabbedoutput(r *Report, w io.Writer) {
-	fmt.Fprintf(w, "\n-----------\nSLO report: (%s) \n-----------\n",
+	fmt.Fprintf(w, "\n-----------\nSLO report: (last %s) \n-----------\n",
 		r.ObservationWindow.To.Sub(r.ObservationWindow.From))
 
 	table := tablewriter.NewWriter(w)
@@ -92,36 +101,40 @@ func tabbedoutput(r *Report, w io.Writer) {
 	var data [][]string
 
 	// set error budget data
-	if r.ServiceLevel.Delta.ErrorPercent < threshold {
+	if r.ServiceLevel.Delta.ErrorPercent > threshold {
 		data = append(data, []string{
-			fmt.Sprintf("%s Error Budget", iconTick),
-			fmt.Sprintf("%.2f", r.ServiceLevel.Actual.ErrorPercent),
+			fmt.Sprintf("%s Error Rate", iconEx),
+			color.Bold(color.Red(fmt.Sprintf("%.2f", r.ServiceLevel.Actual.ErrorPercent))),
+			fmt.Sprintf("%.2f", r.ServiceLevel.Target.ErrorPercent),
+			fmt.Sprintf("%.2f%%", r.ServiceLevel.Delta.ErrorPercent),
+			fmt.Sprintf(errorBudgetExceededf, r.ServiceLevel.Delta.ErrorPercent),
+		})
+	} else {
+		data = append(data, []string{
+			fmt.Sprintf("%s Error Rate", iconTick),
+			color.Bold(color.Green(fmt.Sprintf("%.2f", r.ServiceLevel.Actual.ErrorPercent))),
 			fmt.Sprintf("%.2f", r.ServiceLevel.Target.ErrorPercent),
 			fmt.Sprintf("%.2f%%", -r.ServiceLevel.Delta.ErrorPercent),
 			fmt.Sprintf(errorBudgetTooLowf, -r.ServiceLevel.Delta.ErrorPercent),
 		})
 	}
 
-	if r.ServiceLevel.Delta.ErrorPercent > threshold {
-		// l.Warnf(errorBudgetExceededf, r.ServiceLevel.Delta.ErrorPercent)
-		data = append(data, []string{
-			fmt.Sprintf("%s Error Budget", iconEx),
-			color.Bold(color.Red(fmt.Sprintf("%.2f", r.ServiceLevel.Actual.ErrorPercent))),
-			fmt.Sprintf("%.2f", r.ServiceLevel.Target.ErrorPercent),
-			fmt.Sprintf("%.2f%%", r.ServiceLevel.Delta.ErrorPercent),
-			fmt.Sprintf(errorBudgetExceededf, r.ServiceLevel.Delta.ErrorPercent),
-		})
-	}
-
 	// set latency
 	if r.ServiceLevel.Delta.LatencyMs > threshold {
-		// l.Warnf(latencyExceeded, r.ServiceLevel.Delta.LatencyMs)
 		data = append(data, []string{
 			fmt.Sprintf("%s Latency", iconEx),
 			color.Bold(color.Red(fmt.Sprintf("%dms", r.ServiceLevel.Actual.LatencyMs))),
 			fmt.Sprintf("%dms", r.ServiceLevel.Target.LatencyMs),
 			fmt.Sprintf("%dms", r.ServiceLevel.Delta.LatencyMs),
 			fmt.Sprintf(latencyExceeded, r.ServiceLevel.Delta.LatencyMs),
+		})
+	} else {
+		data = append(data, []string{
+			fmt.Sprintf("%s Latency", iconTick),
+			color.Bold(color.Green(fmt.Sprintf("%dms", r.ServiceLevel.Actual.LatencyMs))),
+			fmt.Sprintf("%dms", r.ServiceLevel.Target.LatencyMs),
+			fmt.Sprintf("%dms", r.ServiceLevel.Delta.LatencyMs),
+			latencyValid,
 		})
 	}
 
