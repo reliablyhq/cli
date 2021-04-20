4acdf11707dd1c473dae962dcfcbab1850e47f14
David Martin <david@chaosiq.io>
2021-04-20 10:28:40 +0200

    add unit tests
    Signed-off-by: David Martin <david@chaosiq.io>

--- core/metrics/aws/api_gateway_test.go
+++ core/metrics/aws/api_gateway_test.go
@@ -1,7 +1,9 @@
 package aws
 
 import (
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/arn"
@@ -23,4 +25,56 @@ func TestApiGWDimension(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotEqual(t, nil, dim, "Dimension has not been returned")
 	assert.Equal(t, "az12er34ty56", aws.ToString(dim.Value), "Unexpected ApiGateway ID from dimension")
+}
+
+func TestApiGWGetMetricQueryInputs(t *testing.T) {
+
+	to := time.Now()
+	from := to.Add(-oneDay)
+	arn, _ := arn.Parse(arnELB)
+
+	apigw := &ApiGateway{}
+
+	type args []interface{}
+	tests := []struct {
+		name string
+		f    reflect.Value
+		args args
+	}{
+		{
+			name: "error rate",
+			f:    reflect.ValueOf(apigw.GetErrorRateMetricDataInput),
+			args: args{
+				arn, from, to,
+			},
+		},
+		{
+			name: "p99 latency",
+			f:    reflect.ValueOf(apigw.GetLatencyMetricDataInput),
+			args: args{
+				arn, from, to,
+			},
+		},
+		{
+			name: "latency above threshold",
+			f:    reflect.ValueOf(apigw.GetLatencyAboveThresholdPerMin),
+			args: args{
+				arn, from, to, 250.0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var args []reflect.Value
+			for _, x := range tt.args {
+				args = append(args, reflect.ValueOf(x))
+			}
+
+			values := tt.f.Call(args)
+
+			m, err := values[0], values[1]
+			assert.Equal(t, nil, err.Interface(), "Unexpected error")
+			assert.NotEqual(t, nil, m, "Metric Query should not be nil")
+		})
+	}
 }
--- core/metrics/aws/load_balancer_test.go
+++ core/metrics/aws/load_balancer_test.go
@@ -1,10 +1,13 @@
 package aws
 
 import (
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/arn"
+	//"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -23,4 +26,56 @@ func TestELBDimension(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotEqual(t, nil, dim, "Dimension has not been returned")
 	assert.Equal(t, "app/dummy/az12er34ty56", aws.ToString(dim.Value), "Unexpected ELB ID from dimension")
+}
+
+func TestELBGetMetricQueryInputs(t *testing.T) {
+
+	to := time.Now()
+	from := to.Add(-oneDay)
+	arn, _ := arn.Parse(arnELB)
+
+	elb := &ElasticLoadBalancer{}
+
+	type args []interface{}
+	tests := []struct {
+		name string
+		f    reflect.Value
+		args args
+	}{
+		{
+			name: "error rate",
+			f:    reflect.ValueOf(elb.GetErrorRateMetricDataInput),
+			args: args{
+				arn, from, to,
+			},
+		},
+		{
+			name: "p99 latency",
+			f:    reflect.ValueOf(elb.GetLatencyMetricDataInput),
+			args: args{
+				arn, from, to,
+			},
+		},
+		{
+			name: "latency above threshold",
+			f:    reflect.ValueOf(elb.GetLatencyAboveThresholdPerMin),
+			args: args{
+				arn, from, to, 250.0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var args []reflect.Value
+			for _, x := range tt.args {
+				args = append(args, reflect.ValueOf(x))
+			}
+
+			values := tt.f.Call(args)
+
+			m, err := values[0], values[1]
+			assert.Equal(t, nil, err.Interface(), "Unexpected error")
+			assert.NotEqual(t, nil, m, "Metric Query should not be nil")
+		})
+	}
 }
