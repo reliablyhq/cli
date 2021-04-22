91ae0d26fdc21d4e776e079d558cee5ab20fef01
Marc Perrien <marc@chaosiq.io>
2021-04-22 14:06:33 +0200

    Move AWS- and GCP-related functions into ad hoc files. Cf PR review.
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

--- cmd/reliably/slo/init/cmd.go
+++ cmd/reliably/slo/init/cmd.go
@@ -1,27 +1,15 @@
 package init
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"os"
 	"sort"
 	"strings"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/apigatewayv2"
-	elb "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
-	"github.com/aws/aws-sdk-go-v2/service/iam"
-	"github.com/aws/aws-sdk-go/aws/arn"
-	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/reliablyhq/cli/core/cli/question"
 	"github.com/reliablyhq/cli/core/color"
 	"github.com/reliablyhq/cli/core/manifest"
 	"github.com/spf13/cobra"
-
-	crm "google.golang.org/api/cloudresourcemanager/v3"
-	compute "google.golang.org/api/compute/v1"
 	"gopkg.in/yaml.v2"
 )
 
@@ -105,8 +93,6 @@ func populateManifestInteractively(m *manifest.Manifest) {
 
 	var s manifest.Service
 
-	// s.Type = manifest.Service.Type{}
-
 	s.Name = question.WithStringAnswer("What is the name of the service you want to declare SLOs for?")
 
 	declareSLOForService(&s)
@@ -168,284 +154,12 @@ func declareSLOForService(s *manifest.Service) {
 func getResourceIDForProvider(provider string) string {
 	switch provider {
 	case "aws":
-		resourceArn := question.WithStringAnswer("Paste an AWS ARN, or type \"i\" for interactive mode.")
-		if resourceArn == "i" {
-			resolver := endpoints.DefaultResolver()
-			partitions := resolver.(endpoints.EnumPartitions).Partitions()
-
-			if len(partitions) == 0 {
-				fmt.Println(iconWarn, "Reliably couldn't query AWS. Please try again or use normal mode.")
-				return ""
-			}
-
-			var partitionsIDs = []string{}
-			for _, p := range partitions {
-				partitionsIDs = append(partitionsIDs, p.ID())
-			}
-			partitionID := question.WithSingleChoiceAnswer("Select an AWS partition.", partitionsIDs...)
-
-			var partition endpoints.Partition
-
-			for _, p := range partitions {
-				if p.ID() == partitionID {
-					partition = p
-				}
-			}
-
-			if len(partition.Regions()) == 0 {
-				if len(partitions) == 0 {
-					fmt.Println(iconWarn, "Reliably couldn't query AWS. Please try again or use normal mode.")
-					return ""
-				}
-			}
-			var regionsIDs = []string{}
-			for id := range partition.Regions() {
-				regionsIDs = append(regionsIDs, id)
-			}
-			sort.Strings(regionsIDs)
-			regionID := question.WithSingleChoiceAnswer("Select an AWS region.", regionsIDs...)
-
-			cfgIAM, err := config.LoadDefaultConfig(context.TODO())
-			if err != nil {
-				fmt.Println("⚠️ Reliably encountered a problem. Please try again or use normal mode.")
-				return ""
-			}
-			clientIAM := iam.NewFromConfig(
-				cfgIAM,
-				func(opt *iam.Options) {
-					opt.Region = regionID
-				},
-			)
-			iamParams := &iam.GetUserInput{}
-			user, err := clientIAM.GetUser(context.TODO(), iamParams)
-			if err != nil {
-				fmt.Println(iconWarn, "Reliably couldn't authenticate you with AWS. Make sure you are logged in to AWS.")
-				return ""
-			}
-			userArnStr := aws.ToString(user.User.Arn)
-			userArn, err := arn.Parse(userArnStr)
-			if err != nil {
-				fmt.Println(iconWarn, "Reliably encountered a problem. Please try again or use normal mode.")
-				return ""
-			}
-			accountID := userArn.AccountID
-
-			awsServices := []string{}
-			for key := range awsServicesMap {
-				awsServices = append(awsServices, key)
-			}
-			sort.Strings(awsServices) // sorts slice in-place
-			serviceFullName := question.WithSingleChoiceAnswer("Select an AWS service.", awsServices...)
-			service := awsServicesMap[serviceFullName]
-
-			serviceID := selectAWSService(service, regionID)
-
-			if serviceID == "" {
-				return ""
-			} else {
-				resourceArn = "arn:" + partitionID + ":" + service + ":" + regionID + ":" + accountID + ":" + serviceID
-			}
-		}
-		return resourceArn
+		return buildAWSArn()
 	case "gcp":
-		var projectID string
-		var sanitizedResourceType string
-		var resourceName string
-
-		ctx := context.Background()
-		crmService, err := crm.NewService(ctx)
-		orgsService := crm.NewOrganizationsService(crmService)
-		orgs, err := orgsService.Search().Context(ctx).Do()
-		if err != nil {
-			handleGCPError(err)
-			return ""
-		}
-		var orgsList = []string{}
-		orgsMap := make(map[string]string)
-
-		if len(orgs.Organizations) > 0 {
-			for _, o := range orgs.Organizations {
-				displayName := o.DisplayName
-				id := o.Name
-				orgsList = append(orgsList, displayName)
-				orgsMap[displayName] = id
-			}
-			orgDisplayName := question.WithSingleChoiceAnswer("Select an Organization.", orgsList...)
-			orgID := orgsMap[orgDisplayName]
-
-			projectsService := crm.NewProjectsService(crmService)
-			projects, err := projectsService.List().Context(ctx).Parent(orgID).Do()
-			if err != nil {
-				handleGCPError(err)
-				return ""
-			}
-			if len(projects.Projects) == 0 {
-				fmt.Println("Reliably couldn't find any project. Check you have all the required permissions.")
-				return ""
-			}
-			var projectsList = []string{}
-			projectsMap := make(map[string]string)
-			for _, p := range projects.Projects {
-				displayName := p.DisplayName
-				id := p.ProjectId
-				fullName := displayName + " (" + id + ")"
-				projectsList = append(projectsList, fullName)
-				projectsMap[fullName] = id
-			}
-			// TODO Handle empty list case
-			projectFullName := question.WithSingleChoiceAnswer("Select an Project.", projectsList...)
-			projectID = projectsMap[projectFullName]
-
-			resourceType := question.WithSingleChoiceAnswer("What is the 'type' of the resource?", googleResourceTypes...)
-			sanitizedResourceType = sanitizeString(resourceType)
-
-			lbctx := context.Background()
-			computeService, err := compute.NewService(lbctx)
-			lbsService := compute.NewUrlMapsService(computeService)
-			lbs, err := lbsService.List(projectID).Context(ctx).Do()
-			if err != nil {
-				handleGCPError(err)
-				return ""
-			}
-			if len(lbs.Items) > 0 {
-				var lbsList = []string{}
-				for _, lb := range lbs.Items {
-					name := lb.Name
-					lbsList = append(lbsList, name)
-				}
-				resourceName = question.WithSingleChoiceAnswer("Select a resource.", lbsList...)
-			} else {
-				fmt.Println(iconWarn, "Reliably couldn't find matching resources.")
-				fmt.Println("Cancelling.")
-				return ""
-			}
-
-		} else {
-			fmt.Println(iconWarn, "Reliably couldn't list your GCP Organizations.")
-			fmt.Println("For interactive mode to work, you need to ensure the following conditions are met:")
-			fmt.Println(" - You are currently logged in to Google Cloud with the `gcloud` CLI.")
-			fmt.Println(" - The currently-logged user has the `resourcemanager.projects.list` rights on the organization you're working on.")
-			fmt.Println(" - The Cloud Resource Manager API is activated on the projects for which you want to list resources.")
-
-			var insufficientGCPRightsOptions = []string{
-				"Manually add resource (you will be asked to provide the project ID and resource name",
-				"Cancel this resource",
-			}
-			insufficientGCPRights := question.WithSingleChoiceAnswer("What do you want to do now?", insufficientGCPRightsOptions...)
-
-			if insufficientGCPRights == "Cancel this resource" {
-				return ""
-			} else {
-				projectID = question.WithStringAnswer("Enter the Project ID:")
-				resourceType := question.WithSingleChoiceAnswer("What is the 'type' of the resource?", googleResourceTypes...)
-				sanitizedResourceType = sanitizeString(resourceType)
-				resourceName = question.WithStringAnswer("Enter the Resource name:")
-			}
-		}
-
-		resourceID := fmt.Sprintf("%s/%s/%s", projectID, sanitizedResourceType, resourceName)
-
-		return resourceID
-
+		return buildGCPResourceID()
 	default:
 		return question.WithStringAnswer("What is the ID of the resource? This could be the AWS ARN, azure resource ID, etc.")
 	}
-}
-
-func selectAWSService(serviceType string, region string) string {
-	config, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	switch serviceType {
-	case "apigateway":
-		agwClient := apigatewayv2.NewFromConfig(config)
-		output, err := agwClient.GetApis(
-			context.TODO(),
-			&apigatewayv2.GetApisInput{
-				MaxResults: aws.String("50"),
-				// NextToken: aws.String("2"),
-			},
-			func(opt *apigatewayv2.Options) {
-				opt.Region = region
-			},
-		)
-		if err != nil {
-			fmt.Println(iconWarn, "Reliably encountered a problem. Please try again or use normal mode.")
-			return ""
-		}
-
-		if len(output.Items) == 0 {
-			fmt.Println(iconWarn, "Reliably couldn't find any available resources.")
-			return ""
-		}
-		var agwApis = []string{}
-		agwApisMap := make(map[string]string)
-		for _, api := range output.Items {
-			name := aws.ToString(api.Name)
-			ID := aws.ToString(api.ApiId)
-			niceName := name + " (" + ID + ")"
-			agwApis = append(agwApis, niceName)
-			agwApisMap[niceName] = ID
-		}
-		apiNiceName := question.WithSingleChoiceAnswer("Select a Resource.", agwApis...)
-		apiID := agwApisMap[apiNiceName]
-
-		return "/apis/" + apiID
-	case "elasticloadbalancing":
-		elbClient := elb.NewFromConfig(config)
-		output, err := elbClient.DescribeLoadBalancers(
-			context.TODO(),
-			&elb.DescribeLoadBalancersInput{
-				// PageSize: aws.String("50"),
-				// NextToken:,
-			},
-			func(opt *elb.Options) {
-				opt.Region = region
-			},
-		)
-		if err != nil {
-			fmt.Println(iconWarn, "Reliably encountered a problem. Please try again or use normal mode.")
-			return ""
-		}
-		if len(output.LoadBalancers) == 0 {
-			fmt.Println(iconWarn, "Reliably couldn't find any available resources.")
-			return ""
-		}
-		var elbs = []string{}
-		elbsMap := make(map[string]string)
-		for _, lb := range output.LoadBalancers {
-			elbArn := aws.ToString(lb.LoadBalancerArn)
-			name := aws.ToString(lb.LoadBalancerName)
-			parsedArn, err := arn.Parse(elbArn)
-			if err != nil {
-				fmt.Println(iconWarn, "Reliably encountered a problem. Please try again or use normal mode.")
-				return ""
-			}
-			elbID := parsedArn.Resource
-			elbSlice := strings.Split(elbID, "/")
-			elb := elbSlice[len(elbSlice)-1]
-			niceName := name + " (" + elb + ")"
-			elbs = append(elbs, niceName)
-			elbsMap[niceName] = elbID
-		}
-		elbNiceName := question.WithSingleChoiceAnswer("Select a Resource.", elbs...)
-		elbID := elbsMap[elbNiceName]
-		return elbID
-	default:
-		fmt.Println(iconWarn, "Reliably encountered a problem. Please try again or use normal mode.")
-		return ""
-	}
-}
-
-func handleGCPError(err error) {
-	errString := fmt.Sprintf("%+v\n", err)
-	errStringSlice := strings.Split(errString, ".,")
-	errStringNoSuffix := errStringSlice[0] + "."
-	cleanErrString := strings.TrimPrefix(errStringNoSuffix, "googleapi: ")
-	fmt.Println(color.Bold(color.Red(iconWarn, "GCP Error:")))
-	fmt.Printf("%s\n", cleanErrString)
 }
 
 func sanitizeString(s string) string {
