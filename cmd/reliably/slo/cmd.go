b19a158740888fc4503c98c84d11ce42e0fc5667
daidokoro <shaun@daidokoro.io>
2021-04-26 16:23:09 +0100

    added apply/pull commands

--- cmd/reliably/slo/cmd.go
+++ cmd/reliably/slo/cmd.go
@@ -2,7 +2,9 @@ package slo
 
 import (
 	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/reliablyhq/cli/cmd/reliably/slo/apply"
 	init_cmd "github.com/reliablyhq/cli/cmd/reliably/slo/init"
+	"github.com/reliablyhq/cli/cmd/reliably/slo/pull"
 	"github.com/reliablyhq/cli/cmd/reliably/slo/report"
 	"github.com/spf13/cobra"
 )
@@ -16,6 +18,8 @@ func NewCommand() *cobra.Command {
 
 	cmd.AddCommand(init_cmd.NewCommand())
 	cmd.AddCommand(report.NewCommand())
+	cmd.AddCommand(pull.NewCommand())
+	cmd.AddCommand(apply.NewCommand())
 
 	return &cmd
 }
