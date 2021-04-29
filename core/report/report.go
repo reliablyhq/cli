0c1d34b31d4e0a0687684d53c2db9ab717feb954
David Martin <david@chaosiq.io>
2021-04-29 11:56:10 +0200

    change the output name of the json key
    Signed-off-by: David Martin <david@chaosiq.io>

--- core/report/report.go
+++ core/report/report.go
@@ -46,5 +46,5 @@ type ServiceLevelResult struct {
 	Delta  interface{} `json:"delta"`
 
 	// used to record whether the SLO is met or not
-	SloIsMet bool `json:"met"`
+	SloIsMet bool `json:"slo_is_met"`
 }
