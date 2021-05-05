3676a76ee5592afd73bf5872467c3f59ebddc7bf
David Martin <david@chaosiq.io>
2021-05-05 11:56:08 +0200

    parse response from api to get report id when sending a report
    Signed-off-by: David Martin <david@chaosiq.io>

--- api/reports.go
+++ api/reports.go
@@ -6,17 +6,29 @@ import (
 	"fmt"
 	"net/http"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/reliablyhq/cli/core"
 	"github.com/reliablyhq/cli/core/report"
 )
 
-func SendReport(client *Client, orgID string, r *report.Report) error {
+func SendReport(client *Client, orgID string, r *report.Report) (string, error) {
 	path := fmt.Sprintf("orgs/%s/reports", orgID)
 
 	var body bytes.Buffer
 	if err := json.NewEncoder(&body).Encode(r); err != nil {
-		return fmt.Errorf("failed to serialize: %s", err)
+		return "", fmt.Errorf("failed to serialize: %s", err)
 	}
 
-	return client.REST(core.Hostname(), http.MethodPost, path, &body, r)
+	type savedReport struct {
+		ID string `json:"id"`
+	}
+	var response *savedReport
+
+	err := client.REST(core.Hostname(), http.MethodPost, path, &body, &response)
+	if err == nil {
+		log.Debugf("Report %s has been saved", response.ID)
+	}
+
+	return response.ID, err
 }
