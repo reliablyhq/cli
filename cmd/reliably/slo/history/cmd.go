2a4bf1187d2009558d5daaec8e9c2c925fd13bcd
David Martin <david@chaosiq.io>
2021-05-08 11:54:01 +0200

    Add heatmaps
    Signed-off-by: David Martin <david@chaosiq.io>

--- cmd/reliably/slo/history/cmd.go
+++ cmd/reliably/slo/history/cmd.go
@@ -3,7 +3,7 @@ package history
 import (
 	//"errors"
 	"fmt"
-	"math/rand"
+	//"math/rand"
 	"net"
 	"net/http"
 	"os"
@@ -136,6 +136,8 @@ func openHistoryInWebBrowser(reports []report.Report) (err error) {
 
 		var r report.Report = reports[0]
 
+		globalSloAreMet := make([][]bool, 0)
+
 		for i, svc := range r.Services {
 
 			svcDates := make([]string, 0)
@@ -144,6 +146,8 @@ func openHistoryInWebBrowser(reports []report.Report) (err error) {
 			}
 
 			svcSloValues := make(map[string][]opts.LineData, 0)
+
+			svcSloMet := make(map[string][]bool, 0)
 
 			//svc.Name
 
@@ -177,6 +181,22 @@ func openHistoryInWebBrowser(reports []report.Report) (err error) {
 				}
 
 				svcSloValues[sl.Name] = items
+
+				/*
+					hmitems := make([]opts.HeatMapData, 0)
+					for i := 0; i < len(sloMet); i++ {
+						if sloMet[i] {
+							hmitems = append(hmitems, opts.HeatMapData{Value: [1]interface{}{1}})
+						} else {
+							hmitems = append(hmitems, opts.HeatMapData{Value: [1]interface{}{0}})
+						}
+
+						//hmitems = append(hmitems, opts.HeatMapData{Value: sloMet[i]})
+					}
+				*/
+				svcSloMet[sl.Name] = sloMet
+
+				globalSloAreMet = append(globalSloAreMet, sloMet)
 
 				thresholds := make([]opts.LineData, 0)
 				for i := 0; i < len(sloObjectives); i++ {
@@ -271,6 +291,216 @@ func openHistoryInWebBrowser(reports []report.Report) (err error) {
 			}
 			line.Render(w)
 
+			// HEATMAP for SLO per service
+
+			sloNames := make([]string, 0)
+			for k := range svcSloMet {
+				sloNames = append(sloNames, k)
+			}
+
+			fmt.Println("slo names", sloNames)
+
+			xaxis := [...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+
+			/*
+				hm := charts.NewHeatMap()
+				hm.SetGlobalOptions(
+					charts.WithTitleOpts(opts.Title{
+						Title: fmt.Sprintf("heatmap of SLOs for service %s", svc.Name),
+					}),
+					charts.WithXAxisOpts(opts.XAxis{
+						Type:      "dates",
+						Data:      xaxis,
+						SplitArea: &opts.SplitArea{Show: true},
+					}),
+					charts.WithYAxisOpts(opts.YAxis{
+						Type:      "SLOs",
+						Data:      sloNames,
+						SplitArea: &opts.SplitArea{Show: true},
+					}),
+					charts.WithVisualMapOpts(opts.VisualMap{
+						Calculable: false,
+						Min:        0,
+						Max:        1,
+						InRange: &opts.VisualMapInRange{
+							Color: []string{"#50a3ba", "#eac736"},
+						},
+					}),
+				)
+			*/
+
+			hm := charts.NewHeatMap()
+			hm.SetGlobalOptions(
+				charts.WithTitleOpts(opts.Title{
+					Title: fmt.Sprintf("heatmap of SLOs for service %s", svc.Name),
+				}),
+				charts.WithXAxisOpts(opts.XAxis{
+					Type:      "category",
+					Data:      xaxis,
+					SplitArea: &opts.SplitArea{Show: true},
+				}),
+				charts.WithYAxisOpts(opts.YAxis{
+					Type:      "category",
+					Data:      sloNames,
+					SplitArea: &opts.SplitArea{Show: true},
+				}),
+				charts.WithVisualMapOpts(opts.VisualMap{
+					Calculable: true,
+					Min:        0,
+					Max:        1,
+					InRange: &opts.VisualMapInRange{
+						Color: []string{"#7db584", "#d94e5d"},
+					},
+				}),
+			)
+
+			//hm.SetXAxis(dayHrs).AddSeries("heatmap", genHeatMapData())
+
+			//hm.SetXAxis([...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+
+			//allSvcSloMet := make([]opts.HeatMapData, 0)
+
+			hmitems := make([]opts.HeatMapData, 0)
+
+			for ii, name := range sloNames {
+				met := svcSloMet[name]
+
+				for jj, b := range met {
+					var v int = 0
+					if b {
+						v = 1
+					}
+
+					hmitems = append(hmitems, opts.HeatMapData{Value: [3]interface{}{jj, ii, v}})
+				}
+				//allSvcSloMet
+
+			}
+
+			/*
+
+				for i := 0; i < len(sloMet); i++ {
+					if sloMet[i] {
+						hmitems = append(hmitems, opts.HeatMapData{Value: [1]interface{}{1}})
+					} else {
+						hmitems = append(hmitems, opts.HeatMapData{Value: [1]interface{}{0}})
+					}
+
+					//hmitems = append(hmitems, opts.HeatMapData{Value: sloMet[i]})
+				}
+			*/
+
+			/*
+				for _, sloMet := range svcSloMet {
+					allSvcSloMet = append(allSvcSloMet, sloMet...)
+					//fmt.Println("HEATMAP values for", sloName, sloMet)
+
+					//line.AddSeries(sloName, sloItems)
+				}
+			*/
+
+			//hm.AddSeries("heatmap", allSvcSloMet)
+			//fmt.Println("HEATMAP all values", allSvcSloMet)
+			hm.AddSeries("heatmap", hmitems)
+
+			_ = hm.Render(w)
+
+			//hm.Validate()
+
+		}
+
+		// GLOBAL HEATMAP, for all SLOs, for all Services
+
+		xaxis := [...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+
+		hmitems := make([]opts.HeatMapData, 0)
+		hm := charts.NewHeatMap()
+		hm.SetGlobalOptions(
+			charts.WithTitleOpts(opts.Title{
+				Title: "heatmap of all SLOs",
+			}),
+			charts.WithXAxisOpts(opts.XAxis{
+				Name:      "reports",
+				Type:      "category",
+				Data:      xaxis,
+				SplitArea: &opts.SplitArea{Show: true},
+			}),
+			charts.WithYAxisOpts(opts.YAxis{
+				Name: "SLO",
+				Type: "category",
+				//Data:      sloNames,
+				SplitArea: &opts.SplitArea{Show: true},
+			}),
+			charts.WithVisualMapOpts(opts.VisualMap{
+				Calculable: true,
+				Min:        0,
+				Max:        1,
+				InRange: &opts.VisualMapInRange{
+					Color: []string{"#7db584", "#d94e5d"},
+				},
+			}),
+		)
+
+		//hm.SetXAxis(dayHrs).AddSeries("heatmap", genHeatMapData())
+
+		//hm.SetXAxis([...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+
+		//allSvcSloMet := make([]opts.HeatMapData, 0)
+
+		for ii, met := range globalSloAreMet {
+			//met := svcSloMet[name]
+
+			if len(met) == 0 {
+				for jj := 0; jj < 10; jj++ {
+					hmitems = append(hmitems, opts.HeatMapData{Value: [3]interface{}{jj, ii, "-"}})
+				}
+			}
+
+			for jj, b := range met {
+				var v int = 0
+				if b {
+					v = 1
+				}
+
+				hmitems = append(hmitems, opts.HeatMapData{Value: [3]interface{}{jj, ii, v}})
+			}
+			//allSvcSloMet
+
+		}
+
+		/*
+
+			for i := 0; i < len(sloMet); i++ {
+				if sloMet[i] {
+					hmitems = append(hmitems, opts.HeatMapData{Value: [1]interface{}{1}})
+				} else {
+					hmitems = append(hmitems, opts.HeatMapData{Value: [1]interface{}{0}})
+				}
+
+				//hmitems = append(hmitems, opts.HeatMapData{Value: sloMet[i]})
+			}
+		*/
+
+		/*
+			for _, sloMet := range svcSloMet {
+				allSvcSloMet = append(allSvcSloMet, sloMet...)
+				//fmt.Println("HEATMAP values for", sloName, sloMet)
+
+				//line.AddSeries(sloName, sloItems)
+			}
+		*/
+
+		//fmt.Println("LEN", len(xaxis), len(sloNames), len(hmitems))
+
+		//hm.AddSeries("heatmap", allSvcSloMet)
+		//fmt.Println("HEATMAP all values", allSvcSloMet)
+		hm.AddSeries("heatmap", hmitems)
+		fmt.Println("HEATMAP all values", len(hmitems), hmitems)
+
+		fmt.Println()
+		err := hm.Render(w)
+		if err != nil {
+			fmt.Println("ERRROR for heatmap", err)
 		}
 
 		//httpserver(w, req)
@@ -304,6 +534,7 @@ func openHistoryInWebBrowser(reports []report.Report) (err error) {
 	return
 }
 
+/*
 // generate random data for line chart
 func generateLineItems() []opts.LineData {
 	items := make([]opts.LineData, 0)
@@ -312,7 +543,9 @@ func generateLineItems() []opts.LineData {
 	}
 	return items
 }
+*/
 
+/*
 func httpserver(w http.ResponseWriter, _ *http.Request) {
 	// create a new line instance
 	line := charts.NewLine()
@@ -348,3 +581,5 @@ func httpserver(w http.ResponseWriter, _ *http.Request) {
 	line2.Render(w)
 
 }
+
+*/
