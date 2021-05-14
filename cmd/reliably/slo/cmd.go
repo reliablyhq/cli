c8345cd2dea47b42a5a76ce1ab474eccaa284834
daidokoro <shaun@daidokoro.io>
2021-05-14 10:42:23 +0100

    removed pull/appply/edit command registration

--- cmd/reliably/slo/cmd.go
+++ cmd/reliably/slo/cmd.go
@@ -2,10 +2,7 @@ package slo
 
 import (
 	"github.com/MakeNowJust/heredoc/v2"
-	"github.com/reliablyhq/cli/cmd/reliably/slo/apply"
-	"github.com/reliablyhq/cli/cmd/reliably/slo/edit"
 	init_cmd "github.com/reliablyhq/cli/cmd/reliably/slo/init"
-	"github.com/reliablyhq/cli/cmd/reliably/slo/pull"
 	"github.com/reliablyhq/cli/cmd/reliably/slo/report"
 	"github.com/spf13/cobra"
 )
@@ -19,10 +16,6 @@ func NewCommand() *cobra.Command {
 
 	cmd.AddCommand(init_cmd.NewCommand())
 	cmd.AddCommand(report.NewCommand())
-	cmd.AddCommand(pull.NewCommand())
-	cmd.AddCommand(apply.NewCommand())
-	cmd.AddCommand(edit.NewCommand())
-
 	return &cmd
 }
 
