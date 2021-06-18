cfb755adfb13e70d2ebba09022ce853a5b57cada
David Martin <david@chaosiq.io>
2021-06-18 11:49:32 +0200

    Handle auth host when token is provied by env var (without config) - fixes #346
    Signed-off-by: David Martin <david@chaosiq.io>

--- config/hostname.go
+++ config/hostname.go
@@ -1,16 +1,24 @@
 package config
 
+// GetKnownHosts returns the list of hostnames defined in the config auths block
+// If Token is provided by the env var, we use the overiddable host if not found in the config
 func GetKnownHosts() ([]string, error) {
 	cfg, err := readConfigFile()
 	if err != nil {
 		return nil, err
 	}
 
-	i := 0
-	hosts := make([]string, len(cfg.AuthInfo))
-	for key := range cfg.AuthInfo {
-		hosts[i] = key
-		i++
+	hosts := []string{}
+	hasCurrentHost := false
+	for h := range cfg.AuthInfo {
+		hosts = append(hosts, h)
+		if h == Hostname {
+			hasCurrentHost = true
+		}
+	}
+
+	if AuthTokenProvidedFromEnv() && !hasCurrentHost {
+		hosts = append([]string{Hostname}, hosts...)
 	}
 
 	return hosts, nil
