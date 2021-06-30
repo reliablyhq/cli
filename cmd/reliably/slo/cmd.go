134c9ca24a58eecbe6d6aa2ac6b66e7674fac4ea
daidokoro <shaun@daidokoro.io>
2021-06-30 16:22:53 +0100

    re-added related command

--- cmd/reliably/slo/cmd.go
+++ cmd/reliably/slo/cmd.go
@@ -2,6 +2,7 @@ package slo
 
 import (
 	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/reliablyhq/cli/cmd/reliably/alpha/slo/related"
 	"github.com/reliablyhq/cli/cmd/reliably/slo/agent"
 	init_cmd "github.com/reliablyhq/cli/cmd/reliably/slo/init"
 	"github.com/reliablyhq/cli/cmd/reliably/slo/report"
@@ -20,6 +21,7 @@ func NewCommand() *cobra.Command {
 	cmd.AddCommand(init_cmd.NewCommand(nil))
 	cmd.AddCommand(report.NewCommand(nil))
 	cmd.AddCommand(sync.NewCommand(nil))
+	cmd.AddCommand(related.NewCommand(nil))
 	return &cmd
 }
 
