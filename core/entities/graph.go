09c08132bd9cb8cb08658229089d5ad90dfb9d88
daidokoro <shaun@daidokoro.io>
2021-06-30 15:43:34 +0100

    added custom unmarshal for nodeGraph type
    Signed-off-by: daidokoro <shaun@daidokoro.io>

--- core/entities/graph.go
+++ core/entities/graph.go
@@ -1,5 +1,7 @@
 package entities
 
+import "encoding/json"
+
 type Node struct {
 	ID       string   `json:"id"`
 	Metadata Metadata `json:"metadata"`
@@ -16,19 +18,26 @@ type NodeGraph struct {
 	Edges []*Edge `json:"edges"`
 }
 
-// custom marshaller to allow marshal indent
-// func (n *NodeGraph) UnmarshalJSON(b []byte) error {
-// 	fmt.Println(n)
-// 	// b, err := json.MarshalIndent(n, "", "  ")
-// 	var ng NodeGraph
-// 	if err := json.Unmarshal(b, &ng); err != nil {
-// 		return err
-// 	}
+/*
+MarshalJSON - custom JSON unmarshaller for NodeGraph type
+used to insure that empty values are returned  instead of null
 
-// 	// make sure edges is not null
-// 	n = &ng
-// 	if n.Edges == nil {
-// 		n.Edges = []*Edge{}
-// 	}
-// 	return nil
-// }
+Eg:
+	{ "nodes": [], "edges": [] }
+	vs.
+	{ "nodes": null, "edges": null }
+*/
+func (g *NodeGraph) MarshalJSON() ([]byte, error) {
+	if g.Edges == nil {
+		g.Edges = []*Edge{}
+	}
+
+	if g.Nodes == nil {
+		g.Nodes = []*Node{}
+	}
+
+	// type alias used to prevent recursive call
+	// to json.Marshal for the same type
+	type alias NodeGraph
+	return json.Marshal(alias(*g))
+}
