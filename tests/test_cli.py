b8f4c48add7307b459853d220d28711f99e0182e
Sylvain Hellegouarch <sh@defuze.org>
2022-11-02 15:16:19 +0100

    add version testing
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 from typer.testing import CliRunner
 
 from reliably_cli.__main__ import cli
+from reliably_cli.__version__ import __version__
 
 runner = CliRunner()
 
@@ -8,3 +9,15 @@ runner = CliRunner()
 def test_app_help():
     result = runner.invoke(cli, ["--help"])
     assert result.exit_code == 0
+
+
+def test_app_version():
+    result = runner.invoke(cli, ["version"])
+    assert result.exit_code == 0
+    assert result.output.strip() == f"Reliably CLI: {__version__}"
+
+
+def test_app_short_version():
+    result = runner.invoke(cli, ["version", "-s"])
+    assert result.exit_code == 0
+    assert result.output.strip() == __version__
