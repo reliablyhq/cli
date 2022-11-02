10e90de69003b6ef196f39e5dc0dceebf1e4edd0
Sylvain Hellegouarch <sh@defuze.org>
2022-11-02 16:53:56 +0100

    Use scm for version
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- reliably_cli/__version__.py
+++ reliably_cli/__version__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.1"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("reliably-cli")
+except PackageNotFoundError:
+    __version__ = 'unknown'
