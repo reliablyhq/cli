7b4d9611b745e7972e09ec92dfc2fc489738b618
Sylvain Hellegouarch <sh@defuze.org>
2022-11-02 16:56:15 +0100

    delint
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- reliably_cli/__version__.py
+++ reliably_cli/__version__.py
@@ -1,6 +1,6 @@
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("reliably-cli")
 except PackageNotFoundError:
-    __version__ = 'unknown'
+    __version__ = "unknown"
